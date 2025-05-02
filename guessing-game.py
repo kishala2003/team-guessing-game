67353bceb0b06f7d2af1f7acfd5117cdea3227a2
kishala2003 <kishalakariyawasam@gmail.com>
2025-05-03 00:17:17 +0530

    Update guessing-game.py

--- guessing-game.py
+++ guessing-game.py
@@ -1,4 +1,6 @@
 import random
+
+while True:
 number = random.randint(1, 100)
 print("Guess a number between 1 and 100")
 guess = int(input())
@@ -7,3 +9,7 @@ if guess == number:
   print("You win!")
 else:
   print(f"Wrong! The number was {number}")
+
+print("Play again? (y/n)")
+    if input().lower() != 'y':
+        break
